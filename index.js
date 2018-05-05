a205092a71b0cb0e933b2aad821f8e6fa9233887
Steven Crouse <sdcrouse91@gmail.com>
2018-05-05 04:40:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ function sayHiToGrandma(string) {
   else if(string.toUpperCase() === string) {
   return "YES INDEED!"
   }
- /* else if(string === "I love you, Grandma.") {
-    return "I love you, too."
-  }*/
-  else {
+ else if(string === "I love you, Grandma.") {
     return "I love you, too."
   }
+  /*else {
+    return "..."
+  }*/
 
 }
